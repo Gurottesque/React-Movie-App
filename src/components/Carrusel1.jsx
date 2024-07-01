4d35532e253b9b3c7a7dcf2be513b91696ecff34
VegaSyd <nicolas.vega@utp.edu.co>
2024-06-30 23:35:03 -0500

    faltaMain

--- src/components/Carrusel1.jsx
+++ src/components/Carrusel1.jsx
@@ -1,52 +1,67 @@
-import { useState, useEffect,useRef } from 'react';
-import { MovieApi } from './MovieApi';
-import '../stylesheets/Recommendations.css';
-import { motion } from 'framer-motion';
-import '../stylesheets/Carrusel1.css'
+import { useState, useEffect, useRef } from "react";
+import { MovieApi } from "./MovieApi";
+import "../stylesheets/Recommendations.css";
+import { motion } from "framer-motion";
+import "../stylesheets/Carrusel1.css";
+import { Link } from "react-router-dom";
 
-const Recommendations = ({tipo}) => {
+const Recommendations = ({ tipo }) => {
   const [entitie, setEntitie] = useState([]);
   const sliderRef = useRef(null);
 
   useEffect(() => {
     const fetchEntitie = async () => {
-        const trendingEntitie = await MovieApi.getData(`trending/${tipo}/week`, '','');
-        console.log(trendingEntitie.results);
-        setEntitie(trendingEntitie.results);
+      const trendingEntitie = await MovieApi.getData(
+        `trending/${tipo}/week`,
+        "",
+        ""
+      );
+      console.log(trendingEntitie.results);
+      setEntitie(trendingEntitie.results);
     };
     fetchEntitie();
   }, []);
 
-
-
-
   return (
-
     <div className="recommendations-container-r">
-      <button className='nav-button2 prev2'  ><img src="./flecha-izq.svg" alt="" /></button>
+      <button className="nav-button2 prev2">
+        <img src="./flecha-izq.svg" alt="" />
+      </button>
       <motion.div className="movies-grid-r">
-        <motion.div className="slider" ref={sliderRef} drag='x' dragConstraints={{right:0,left:-2370}}>
-        {entitie.map((entitie) => (
-          <motion.div key={entitie.id} className="movie-card-r">
-            <img src={tipo==='person' ? MovieApi.getImage(entitie.profile_path): MovieApi.getImage(entitie.poster_path)} alt={entitie.title} />
-            <div className="movie-info-r"> 
-              <h3>{tipo === 'movie' ? entitie.title : entitie.name}</h3>
-              <div className='votos'>
-                <img src="./star.svg" alt="" />
-              <p>{entitie.vote_average}</p>
-              </div>
-            </div>
-          </motion.div>
-        ))}
+        <motion.div
+          className="slider"
+          ref={sliderRef}
+          drag="x"
+          dragConstraints={{ right: 0, left: -2370 }}
+        >
+          {entitie.map((entitie) => (
+            <motion.div key={entitie.id} className="movie-card-r">
+              <Link to={`/details/${tipo}/${entitie.id}`}>
+                <img
+                  src={
+                    tipo === "person"
+                      ? MovieApi.getImage(entitie.profile_path)
+                      : MovieApi.getImage(entitie.poster_path)
+                  }
+                  alt={entitie.title}
+                />
+                <div className="movie-info-r">
+                  <h3>{tipo === "movie" ? entitie.title : entitie.name}</h3>
+                  <div className="votos">
+                    <img src="./star.svg" alt="" />
+                    <p>{entitie.vote_average}</p>
+                  </div>
+                </div>
+              </Link>
+            </motion.div>
+          ))}
         </motion.div>
       </motion.div>
-      <button className='nav-button2 next2' ><img src="./flecha-der.svg" alt="" /></button>
+      <button className="nav-button2 next2">
+        <img src="./flecha-der.svg" alt="" />
+      </button>
     </div>
-    
-    
   );
- 
-}
-
+};
 
 export default Recommendations;
--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,41 +1,38 @@
-import React, { useEffect, useState } from 'react'
-import {useParams} from 'react-router-dom'
-import { MovieApi } from './MovieApi'
-import '../stylesheets/Details.css'
-
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { MovieApi } from "./MovieApi";
+import "../stylesheets/Details.css";
 
 const Details = () => {
   const [detail, setDetail] = useState(null);
   const [imgs, setImgs] = useState(null);
-  const {id} = useParams();
-  const {type} = useParams();
-  
+  const { id } = useParams();
+  const { type } = useParams();
+
   useEffect(() => {
     const fetchMovie = async () => {
-      const detail = await MovieApi.getData(`${type}/${id}`, '', '');
-      const imgs = await MovieApi.getData(`${type}/${id}/images`, '', ''); 
+      const detail = await MovieApi.getData(`${type}/${id}`, "", "");
+      const imgs = await MovieApi.getData(`${type}/${id}/images`, "", "");
       setDetail(detail);
       setImgs(imgs);
-      console.log(detail)
-      console.log(detail.gender !== undefined) //Personas
-      console.log(detail.seassons != undefined) //Series
-
-    }
+      console.log(detail);
+      console.log(detail.gender !== undefined); //Personas
+      console.log(detail.seassons != undefined); //Series
+    };
     fetchMovie();
-
-  },[]);
+  }, []);
 
   return (
-    <div className='main-details-container'>
-      <div className='details-container'>
-        {detail && (
-          detail.gender !== undefined? (//Persons
+    <div className="main-details-container">
+      <div className="details-container">
+        {detail &&
+          (detail.gender !== undefined ? ( //Persons
             <>
               <h1>{detail.name}</h1>
-              <div className='details-img'>
-                <img 
-                  src={MovieApi.getImage(detail.profile_path)} 
-                  alt={detail.name} 
+              <div className="details-img">
+                <img
+                  src={MovieApi.getImage(detail.profile_path)}
+                  alt={detail.name}
                 />
               </div>
               <p>Biography: {detail.overview}</p>
@@ -44,54 +41,66 @@ const Details = () => {
               <p>Place of birth: {detail.place_of_birth}</p>
               <p>Know for: {detail.known_for_department}</p>
             </>
-        ): detail.seasons !== undefined?( //TV SHOWs
-          <>
-            <div 
-            className="title-background" 
-            style={{ backgroundImage: `url(${MovieApi.getImage(detail.backdrop_path)})` }}
-            >
-              <h1>{detail.name}</h1>
-            </div>
-            <div className="details-img">
-            <img 
-              src={MovieApi.getImage(detail.poster_path)} 
-              alt={detail.name} 
-            />
-            </div>
-            <p>Genres: {detail.genres.map(genre => genre.name).join(', ')}</p>
-            <p>Overview: {detail.overview}</p>
-            <p>Popularity: {detail.popularity}</p>
-            <p>First air date: {detail.first_air_date}</p>
-            <p>Last air date: {detail.last_air_date}</p>
-            <p>Number of seasons: {detail.number_of_seasons}</p>
-            <p>Number of episodes: {detail.number_of_episodes}</p>
-          </>
-        
-        ): (//Movies
-          <>
-            <div 
-            className="title-background" 
-            style={{ backgroundImage: `url(${MovieApi.getImage(detail.backdrop_path)})` }}
-            >
-              <h1>{detail.title}</h1>
-            </div>
-            <div className="details-img">
-            <img 
-              src={MovieApi.getImage(detail.poster_path)} 
-              alt={detail.title} 
-            />
-            </div>
-            <p>Genres: {detail.genres.map(genre => genre.name).join(', ')}</p>
-            <p>Overview: {detail.overview}</p>
-            <p>Popularity: {detail.popularity}</p>
-            <p>Release date: {detail.release_date}</p>
-            <p>Runtime: {detail.runtime} minutes</p>
-          </>
-        )
-        )}
-       </div>
-    </div>
-  )
-}
+          ) : detail.seasons !== undefined ? ( //TV SHOWs
+            <>
+              <div
+                className="title-background"
+                style={{
+                  backgroundImage: `url(${MovieApi.getImage(
+                    detail.backdrop_path
+                  )})`,
+                }}
+              >
+                <h1>{detail.name}</h1>
 
-export default Details
+                <div className="details-img">
+                  <img
+                    src={MovieApi.getImage(detail.poster_path)}
+                    alt={detail.name}
+                  />
+                </div>
+              </div>
+              <p>
+                Genres: {detail.genres.map((genre) => genre.name).join(", ")}
+              </p>
+              <p>Overview: {detail.overview}</p>
+              <p>Popularity: {detail.popularity}</p>
+              <p>First air date: {detail.first_air_date}</p>
+              <p>Last air date: {detail.last_air_date}</p>
+              <p>Number of seasons: {detail.number_of_seasons}</p>
+              <p>Number of episodes: {detail.number_of_episodes}</p>
+            </>
+          ) : (
+            //Movies
+            <>
+              <div
+                className="title-background"
+                style={{
+                  backgroundImage: `url(${MovieApi.getImage(
+                    detail.backdrop_path
+                  )})`,
+                }}
+              >
+                <h1>{detail.title}</h1>
+              </div>
+              <div className="details-img">
+                <img
+                  src={MovieApi.getImage(detail.poster_path)}
+                  alt={detail.title}
+                />
+              </div>
+              <p>
+                Genres: {detail.genres.map((genre) => genre.name).join(", ")}
+              </p>
+              <p>Overview: {detail.overview}</p>
+              <p>Popularity: {detail.popularity}</p>
+              <p>Release date: {detail.release_date}</p>
+              <p>Runtime: {detail.runtime} minutes</p>
+            </>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default Details;
